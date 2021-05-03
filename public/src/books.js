c62a1f001ccc43c478f040306cbf0e13d6ec28a5
Whitney Green <81326194+whitneygreen@users.noreply.github.com>
2021-05-03 08:53:37 -0400

    updated books.js
    added notes

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,12 @@
 function findAuthorById(authors, id) {
+    //use find to compare the author id to the id we input. return that author object. 
 let find = authors.find((authorObj)=> authorObj.id === id);
     return find;
 }
 
 
 function findBookById(books, id) {
+       //use find to compare the book id to the id we input. return that book object.
   let find = books.find((bookObj)=> bookObj.id === id);
     return find;
 }
@@ -12,9 +14,11 @@ function findBookById(books, id) {
 
 
 function partitionBooksByBorrowedStatus(books) {
+    //create a new filtered array containing only the books that have been returned
    const checkedOut = books.filter((book) => book.borrows[0].returned);
+    //create a new array containing only the books that have not been returned
   const checkedIn = books.filter((book) => !book.borrows[0].returned);
-
+//return both new arrays within one larger array
 return [checkedIn, checkedOut];
  
 
@@ -24,14 +28,19 @@ return [checkedIn, checkedOut];
 
 
 function getBorrowersForBook(book, accounts) {
+    //pull the borrows object out of the book
   const { borrows } = book;
+    //map over borrows to find the id and the returned status
   const borrowers = borrows.map(({ id, returned })=> {
+      //witin that loop, go over accounts to find the account id that matches the one from borrows
     const account = accounts.find(account => account.id === id);
-    return {
+   //return that account data with the returned data from borrows
+      return {
       ...account,
       returned,
     };
   });
+    //limit the list to 10 items
   return borrowers.slice(0, 10);
 }
 
